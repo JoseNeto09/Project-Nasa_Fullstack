1f8d5a5d8ff34da025907dcc7b378002c7bda5f0
JoseNeto09 <joselopessobrinhoneto9@gmail.com>
2023-04-03 16:12:47 -0300

    first commit

---
+++ server/src/routes/launches/launches.controller.js
@@ -0,0 +1,46 @@
+const { getAllLaunches, addNewLaunch, existsLaunchWithId, abortLaunchById } = require('../../models/launches.model');
+
+function httpGetAllLaunches(req, res) {
+    return res.status(200).json(getAllLaunches());
+}
+
+function httpAddNewLaunch(req, res){
+    const launch = req.body;
+
+    if(!launch.mission || !launch.rocket || !launch.launchDate
+        || !launch.target) {
+        return res.status(400).json({
+            error: 'Missing required launch proprety',
+        });
+    }
+    
+    launch.launchDate = new Date(launch.launchDate);
+    if (isNaN(launch.launchDate)){
+        return res.status(400).json({
+            error: 'invalid launch date',
+        });
+    }
+
+    addNewLaunch(launch);
+    return res.status(201).json(launch);
+}
+
+function httpAbortLaunch(req, res){
+    const launchId = Number(req.params.id);
+
+    //if launch doesent exist
+    if (!existsLaunchWithId(launchId)) {
+      return res.status(404).json({
+        error: 'Launch not found',
+    });
+    }
+
+    const aborted = abortLaunchById(launchId);
+    return res.status(200).json(aborted);
+}
+
+module.exports ={
+    httpGetAllLaunches,
+    httpAddNewLaunch,
+    httpAbortLaunch,
+};
