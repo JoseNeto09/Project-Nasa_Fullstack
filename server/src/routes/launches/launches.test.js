34d78be667dbc99787519581c049fab671d1bbf8
JoseNeto09 <joselopessobrinhoneto9@gmail.com>
2023-04-08 18:17:13 -0300

    testes unitarios

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,16 @@
+describe('Test GET /launches', () =>{
+    test('It should respond with 200 sucess', () =>{
+        const response = 200;
+        expect(response).toBe(200);
+    });
+});
+
+describe('Test Post /launch', () =>{
+    test('It should respond with 200 success', () =>{
+
+    });
+
+    test('It should catch missing required properties', () => {});
+    test('It should catch missing invalid dates', () =>{});
+})
+
